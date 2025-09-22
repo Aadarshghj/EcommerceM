93b4e5aeeba5c3df0d3fceb8eef1f9303a12a390
Aadarshghj <aadarshaadarsh109@gmail.com>
2025-09-22 19:16:53 +0530

    Update server.js debug

--- backend/server.js
+++ backend/server.js
@@ -73,13 +73,11 @@ try {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  // ✅ Use regex instead of "*"
-  app.get("/*", (req, res) => {
+  // ✅ Simple wildcard - catches all routes
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
-
 app.listen(PORT, () => {
 	console.log(`🚀 Server is running on port ${PORT}`);
 });
